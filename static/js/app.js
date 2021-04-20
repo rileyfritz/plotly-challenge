b10420f54b7b77441834ad6d4fc0911c835c04e2
Riley Fritz <rileyfritz5773@gmail.com>
2021-04-20 15:27:35 -0500

    got items in drop down. add chart filter based on dropdown value

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,21 @@
+
+
 d3.json("./data/samples.json").then(function(data) {
     // Load and inspect data
-    console.log(data);
+    // console.log(data);
     
     // Get only the sample data 
     var samples = data.samples;
     console.log(samples);
+    var select = document.getElementById("selDataset");
 
+    for(var i = 0; i < samples.length; i++) {
+        var option = document.createElement('option');
+        option.text = samples[i].id;
+        console.log(option.text);
+        select.add(option, 0);
+    };
+    // addList();
     // filter based on id
     var result = samples.filter(obj => {
         return obj.id === "948"
@@ -32,3 +42,12 @@ d3.json("./data/samples.json").then(function(data) {
     Plotly.newPlot('bar', data);
 });
 
+// function addList() {
+//     var select = document.getElementById("selDataset");
+//     for(var i = 0; i >= samples.length; ++i) {
+//         var option = document.createElement('option');
+//         option.text = option.value = samples[i];
+//         console.log(option.text);
+//         select.add(option, 0);
+//     }
+// };
