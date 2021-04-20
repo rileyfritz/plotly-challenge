cb2220e92c127cfa1b420cfafcd9588487fdf313
Riley Fritz <rileyfritz5773@gmail.com>
2021-04-20 12:05:49 -0500

    got the bar plot working. need to filter by top 10 and sort. also need to format axes

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,31 @@
-var bellyData = d3.json("./data/samples.json");
-console.log(bellyData);
+d3.json("./data/samples.json").then(function(data) {
+    // Load and inspect data
+    console.log(data);
+    
+    // Get only the sample data 
+    var samples = data.samples;
+    console.log(samples);
+
+    // filter based on id
+    var result = samples.filter(obj => {
+        return obj.id === "941"
+        });
+    console.log(result);
+
+    // put chart data in variables
+    var sampleValues = result[0].sample_values;
+    console.log(sampleValues);
+    var otuIDs = result[0].otu_ids;
+    console.log(otuIDs);
+    // var otuLabels = result.otu_labels
+
+    var data = [{
+        type: 'bar',
+        x: sampleValues,
+        y: otuIDs,
+        orientation: 'h'
+      }];
+      
+    Plotly.newPlot('bar', data);
+});
+
