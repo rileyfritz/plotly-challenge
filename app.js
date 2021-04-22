fed163daef365ca97229546b4f86eed11ba03caf
Riley Fritz <rileyfritz5773@gmail.com>
2021-04-22 10:59:08 -0500

    bar chart is working. change so drop down values only entered once. Also flip chart so longest are on top if possible

---
+++ app.js
@@ -0,0 +1,71 @@
+function InitDashboard() {
+
+    d3.json("./data/samples.json").then(function (data) {
+        // Load and inspect data
+        // console.log(data);
+
+        // Get only the sample data 
+        var samples = data.samples;
+        // console.log(samples);
+
+        // Select and populate the dropdwon
+        var select = document.getElementById("selDataset");
+
+        for (var i = 0; i < samples.length; i++) {
+            var option = document.createElement('option');
+            option.text = samples[i].id;
+            // console.log(option.text);
+            select.add(option, 0);
+        };
+
+        // get current dropdown value
+        var dropdownMenu = d3.select("#selDataset");
+        var currentID = dropdownMenu.property("value");
+
+        // filter based on id
+        var result = samples.filter(obj => {
+            return obj.id === currentID
+        });
+
+        // Storing vars for chart data
+        var sampleValues = result[0].sample_values;
+        sampleValues = sampleValues.slice(0, 10); // Add line to reverse so chart is flipped
+        var otuIDs = result[0].otu_ids;
+        var otuLabels = result[0].otu_labels;
+
+        otuIDs.forEach(function (part, index, otuIDs) {
+            otuIDs[index] = `OTU ${otuIDs[index]}`;
+        });
+
+        var data = [{
+            type: 'bar',
+            x: sampleValues,
+            y: otuIDs,
+            text: otuLabels,
+            orientation: 'h'
+        }];
+
+        Plotly.newPlot('bar', data);
+
+    });
+};
+
+InitDashboard();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ data.js
@@ -0,0 +1,2 @@
+// var data = d3.json("./data/samples.json");
+// console.log(data);
